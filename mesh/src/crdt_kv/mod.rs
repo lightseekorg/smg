2fce912b09a875211371a92ddd8bacd5e404bfa0
kun-llfl <i@imux.top>
2026-02-25 08:56:54 +0800

    Update mesh/src/crdt_kv/mod.rs
    Co-authored-by: Tony Lu <tonylu@linux.alibaba.com>

--- mesh/src/crdt_kv/mod.rs
+++ mesh/src/crdt_kv/mod.rs
@@ -10,10 +10,6 @@ mod replica;
 // Export core types
 pub use crdt::CrdtOrMap;
 pub use operation::OperationLog;
-// pub use replica::ReplicaId;
-// pub use kv_store::KvStore;
-// pub use operation::Operation;
-// pub use replica::LamportClock;
 
 #[cfg(test)]
 mod tests;
