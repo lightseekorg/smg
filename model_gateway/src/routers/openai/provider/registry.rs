a673326a599699f4de143a152a4e100e9abbc9cb
Vadivelan Udayakumar <vadivelan.udayakumar@oracle.com>
2026-04-22 19:44:15 +0530

    refactor(openai): remove unused provider registry helpers
    Signed-off-by: Vadivelan Udayakumar <vadivelan.udayakumar@oracle.com>

--- model_gateway/src/routers/openai/provider/registry.rs
+++ model_gateway/src/routers/openai/provider/registry.rs
@@ -43,31 +43,11 @@ impl ProviderRegistry {
         }
     }
 
-    pub fn get(&self, provider_type: &ProviderType) -> &dyn Provider {
-        self.providers
-            .get(provider_type)
-            .map(|p| p.as_ref())
-            .unwrap_or_else(|| self.default_provider.as_ref())
-    }
-
     pub fn get_arc(&self, provider_type: &ProviderType) -> Arc<dyn Provider> {
         self.providers
             .get(provider_type)
             .cloned()
             .unwrap_or_else(|| Arc::clone(&self.default_provider))
-    }
-
-    #[expect(dead_code)]
-    pub fn get_for_model(&self, model_name: &str) -> &dyn Provider {
-        match ProviderType::from_model_name(model_name) {
-            Some(pt) => self.get(&pt),
-            None => self.default_provider.as_ref(),
-        }
-    }
-
-    #[expect(dead_code)]
-    pub fn default_provider(&self) -> &dyn Provider {
-        self.default_provider.as_ref()
     }
 
     pub fn default_provider_arc(&self) -> Arc<dyn Provider> {
