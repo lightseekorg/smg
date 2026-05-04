5e4db4fd898226406d7f6c8f869fb866a54c8929
Pramod Rudrawadi <pramod.rudrawadi@oracle.com>
2026-05-04 18:24:12 +0530

    refactor: code format.
    Signed-off-by: Pramod Rudrawadi <pramod.rudrawadi@oracle.com>

--- model_gateway/src/routers/common/openai_bridge/transformer.rs
+++ model_gateway/src/routers/common/openai_bridge/transformer.rs
@@ -1413,7 +1413,9 @@ mod tests {
         );
 
         match transformed {
-            ResponseOutputItem::FileSearchCall { queries, results, .. } => {
+            ResponseOutputItem::FileSearchCall {
+                queries, results, ..
+            } => {
                 assert!(queries.is_empty());
                 let results = results.expect("expected array fallback results");
                 assert_eq!(results.len(), 1);
